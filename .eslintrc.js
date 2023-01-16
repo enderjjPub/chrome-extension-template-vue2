d01ef1c6aa1df10e730df934cfc41f02e2316793
enderjjPub <chnhongjjPub@163.com>
2023-01-16 18:09:22 +0800

    fix: manifest

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 1,
+    'no-new': 0,
+    'comma-dangle': 0,
+    'vue/no-unused-components': 1,
+    'vue/max-attributes-per-line': 0,
+    'vue/html-indent': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'prefer-promise-reject-errors': 0,
+    camelcase: 1,
+    'vue/no-use-v-if-with-v-for': 0,
+    'space-before-function-paren': 0,
   },
 };
